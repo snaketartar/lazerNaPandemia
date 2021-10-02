7623a54119c857bb9bb5fce32060e05c9621913b
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-02 13:52:13 -0300

    path

--- lazer-na-pandemia/src/router/index.js
+++ lazer-na-pandemia/src/router/index.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Home from '../views/Home.vue'
+import Anime from '../components/Anime.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/anime',
+    name: 'Anime',
+    component: Anime
+  },
 ]
 
 const router = new VueRouter({
