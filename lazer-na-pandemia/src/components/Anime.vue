33d9a7ef40050cf3e423e5e1f3474aa8a415184c
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-02 11:48:10 -0300

    Fetch API

--- lazer-na-pandemia/src/components/Anime.vue
+++ lazer-na-pandemia/src/components/Anime.vue
@@ -1,7 +1,21 @@
 <template>
 </template>
+
 <script>
 export default {
+  data() {
+    return {
+      animeList: [],
+    };
+  },
+  created() {
+    fetch("https://it3zxc-default-rtdb.firebaseio.com/lazer/animes.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.animeList = json;
+      });
+  },
+};
 </script>
 
 <style scoped></style>
