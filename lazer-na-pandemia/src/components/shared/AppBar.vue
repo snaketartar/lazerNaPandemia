dc48152a22f307904660f7f034a40e2e5bb0ef6b
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-02 13:52:21 -0300

    Anime path

--- lazer-na-pandemia/src/components/shared/AppBar.vue
+++ lazer-na-pandemia/src/components/shared/AppBar.vue
@@ -10,12 +10,23 @@
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
+        <router-link to="/">
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
+        </router-link>  
+
+        <router-link to="/anime">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Anime</v-list-item-title>
+          </v-list-item>
+        </router-link>  
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
