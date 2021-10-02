955faca1c778b93c262ed7481a7af2d51e8fb23e
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-02 11:17:06 -0300

    initial commit

--- lazer-na-pandemia/src/views/Home.vue
+++ lazer-na-pandemia/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <hello-world />
+  <h1></h1>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+
 
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
   }
 </script>
